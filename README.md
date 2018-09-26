d2df52f9240870477dd1c32c919accd62e9c2176
V'Ana <vanagavriela@gmail.com>
2018-09-25 22:17:58 -0500

    finalize

---
+++ README.md
@@ -0,0 +1,2 @@
+FriendFinder
+Created during Week 13 of Rutgers Coding Bootcamp. The goal was to create a Friend Finder app (essentially a dating app but for friends) using Node and Express servers. The user enters the page that leads to a survey. When the user submits the survey, they are matched with a "friend" that has the closest match to their personality.
---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+//array that holds all the 'friends' that have taken the survey
+
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'VAna Abbott',
+       profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/1486795_10202025225565032_1307603993_n.jpg?_nc_cat=101&oh=e74c73f362fcd6254199f2aa2e5c2487&oe=5C2DCB04',
+       scores: [
+         "4",
+         "5",
+         "5",
+         "2",
+         "4",
+         "5",
+         "4",
+         "4",
+         "3",
+         "4"
+       ]
+     }
+   ]
+   
+   module.exports = friendList;
